75197f010b28cb4d22737ae6f5e865b3b703b1bd
Sho Ishiwata <81550242+shoxst@users.noreply.github.com>
2021-10-31 12:53:21 +0900

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using UserGroupRole.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserGroupRole.Controllers
+{
+  [Authorize(Roles =("ProductViewers,ProductAdministrators"))]
+  public class ProductsController : Controller
+  {
+    SampleData data;
+
+    public ProductsController(SampleData data)
+    {
+      this.data = data;
+    }
+
+    public ActionResult Index()
+    {
+      return View(data.Products);
+    }
+
+    [Authorize(Roles = ("ProductAdministrators"))]
+    public ActionResult Create()
+    {
+    var viewModel = new ProductViewModel()
+    {
+        Categories = data.Categories
+    };
+    return View(viewModel);
+    }
+
+    [Authorize(Roles = ("ProductAdministrators"))]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind("ProductName", "CategoryId")] ProductViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            data.Products.Add(new Product()
+            {
+            ID = data.Products.Max(p => p.ID) + 1,
+            Name = model.ProductName,
+            Category = data.Categories.FirstOrDefault(c => c.ID == model.CategoryId)
+            });
+            return RedirectToAction("Index");
+        }
+        return View(model);
+    }
+  }
+}
